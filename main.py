d61b252a4f5163c1a6d935e8abc2597486d82f05
YetAnotherMinion <yetanotherminion@gmail.com>
2015-07-21 01:20:05 -0400

    removed unused code

--- main.py
+++ main.py
@@ -244,32 +244,6 @@ def randgradient():
 	response.headers['Content-Type'] = 'image/png'
 	return response
 
-	# from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-	# from matplotlib.figure import Figure
-	# from matplotlib.dates import DateFormatter
-
-	# fig=Figure()
-	# ax=fig.add_subplot(111)
-	# x=[]
-	# y=[]
-	# now=datetime.datetime.now()
-	# delta=datetime.timedelta(days=1)
-	# for i in range(10):
-	# 	x.append(now)
-	# 	now+=delta
-	# 	y.append(random.randint(0, 1000))
-	# ax.plot_date(x, y, '-')
-	# ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
-	# fig.autofmt_xdate()
-	# canvas=FigureCanvas(fig)
-	# png_output = StringIO.StringIO()
-	# canvas.prandint_png(png_output)
-	# response=make_response(png_output.getvalue())
-	# response.headers['Content-Type'] = 'image/png'
-	# return response
-	return "hello world"
-
-
 @app.errorhandler(404)
 def page_not_found(e):
 	return 'Sorry, nothing at this URL.', 404
